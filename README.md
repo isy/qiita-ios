01c1f89c5b51bddce5a680c2eddfe91487127fca
akaishi-hironori <akaishi-hironori@dmm.com>
2019-12-06 20:05:17 +0900

    Add: README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Qiita iOS
+This project is a code sandbox. Learn the latest ios app development using Qiita API.
+
+
+## Setup
+```bash
+./scripts/bootstrap.sh
+```
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+
+# Carthage
+if test ! $(which cathage)
+then
+	echo "Installing cathage.."
+	brew install cathage
+else
+	echo "...Carthage found."
+
