393dafa558edfb7824042fb714d7db9097d02a27
akaishi-hironori <akaishi-hironori@dmm.com>
2019-12-06 19:40:46 +0900

    Initial Commit

---
+++ qiita-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiita-ios
+//
+//  Created by akaishi-hironori on 2019/12/06.
+//  Copyright © 2019 akaishi-hironori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
