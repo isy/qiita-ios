7b72cc5772146dfe929ba9e34d4423f5d18402a4
Hironori Akaishi <ah.gm3622@gmail.com>
2019-12-09 00:08:57 +0900

    Add: Carthage setting

---
+++ qiita-ios/Sources/Scenes/Main/FeaturedViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  FeaturedViewModel.swift
+//  qiita-ios
+//
+//  Created by Hironori Akaishi on 2019/12/08.
+//  Copyright © 2019 akaishi-hironori. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+//import RxCocoa
+//
+//class FeaturedViewModel {
+//    
+//    private let itemModelMaxCount: Int!
+//    
+//    let featuredLists:
+//    
+//    init(data: Data) {
+//        
+//    }
+//}
